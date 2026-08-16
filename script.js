3fd57360fcc90e01fda8202e1538eb8978853bb3
sandeepuppugalla <46489526+sandeepuppugalla@users.noreply.github.com>
2026-08-16 13:55:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-setInterval(()=>{document.getElementById('countdown').innerHTML='Wedding Countdown';},1000)
+function openInvite() {
+    document.getElementById("loader").style.display = "none";
+}
+
+const weddingDate =
+new Date("September 4, 2026 10:35:00").getTime();
+
+function updateCountdown() {
+
+    const now = new Date().getTime();
+
+    const distance = weddingDate - now;
+
+    const days =
+    Math.floor(distance / (1000*60*60*24));
+
+    const hours =
+    Math.floor(
+        (distance %
+        (1000*60*60*24))
+        /
+        (1000*60*60)
+    );
+
+    const minutes =
+    Math.floor(
+        (distance %
+        (1000*60*60))
+        /
+        (1000*60)
+    );
+
+    const seconds =
+    Math.floor(
+        (distance %
+        (1000*60))
+        /
+        1000
+    );
+
+    document.getElementById("countdown").innerHTML =
+    `
+    <div class="time-box">
+        <span>${days}</span>
+        <small>Days</small>
+    </div>
+
+    <div class="time-box">
+        <span>${hours}</span>
+        <small>Hours</small>
+    </div>
+
+    <div class="time-box">
+        <span>${minutes}</span>
+        <small>Minutes</small>
+    </div>
+
+    <div class="time-box">
+        <span>${seconds}</span>
+        <small>Seconds</small>
+    </div>
+    `;
+}
+
+updateCountdown();
+
+setInterval(updateCountdown,1000);
